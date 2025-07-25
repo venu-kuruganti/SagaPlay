61b4d106f436eef0c8061f02760191c99e8e1c13
Venu Kuruganti <35195194+venu-kuruganti@users.noreply.github.com>
2025-07-25 11:05:20 +0530

    Created new projects
    Created Angular App.
    Created API Gateway Microservice
    Created Catalog Service
    Created Notification Service
    CReated Recommendation Service
    Created User Service
    Created Watchlist Service.

---
+++ SagaPlay-Backend/ApiGateway/Controllers/ResponseController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiGateway.Controllers
+{
+    public class ResponseController : Controller
+    {
+        public IActionResult SendResponse()
+        {
+                        
+            return Ok();
+        }
+    }
+}
---
+++ SagaPlay-Backend/ApiGatewayTests/ResponseControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+using Moq;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using ApiGateway.Controllers;
+
+namespace ApiGatewayTests
+{
+    public class ResponseControllerTests
+    {
+        [Fact]
+        public void CallingResponseController_Gives_OkResult()
+        {
+            //Arrange
+            var controller = new ResponseController();
+
+            //Act
+            var result = controller.SendResponse();
+
+            //Assert
+            result.Should().BeOfType<OkResult>();
+        }
+    }
+}
