e5e37971396d719f3dc6c02ec7281f3a5757a946
Venu Kuruganti <35195194+venu-kuruganti@users.noreply.github.com>
2025-10-14 12:59:46 +0530

    Syntax error corrections
    Finished setting up connection strings to outside database. Can deploy to Azure.

---
+++ SagaPlay-Backend/WatchlistService/Migrations/20251014055455_latestmigration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WatchlistService.Migrations
+{
+    /// <inheritdoc />
+    public partial class latestmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
